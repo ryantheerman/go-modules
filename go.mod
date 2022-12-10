2b559ee08fe7be5bf448e085f4bc4690fe22738f
RyanTheerman <rwtheerman@gmail.com>
2022-12-10 15:32:00 -0500

    updated hello module with dependencies to find mod in github repo, changed greeting output to test that the pkg is being imported from the repo not locally

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ryantheerman/go-modules
+
+go 1.19
--- test.go
+++ test.go
@@ -5,6 +5,6 @@ import "fmt"
 // Hello returns a greeting for the named person
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi %v! Welcome!", name)
+	message := fmt.Sprintf("Yo %v! Welcome! This message came from a module in a repo on github, believe it or not", name)
 	return message
 }
